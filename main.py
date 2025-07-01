f673181dce1580685aa4a0b88e6457a5af7d05ef
stdelmer <elmeryachakvilcapumachachavot@gmail.com>
2025-07-01 00:29:14 -0300

    E:Good

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
+from stats import get_num_words
 def get_book_text(ruta):
     with open(ruta) as f:
         file_contents = f.read()
     return file_contents
-def book_return(works):
-    num_work = works.split()
-    num_works = len(num_work) +75762
-    print(f"{num_works} words found in the document")
-    return num_works
+
 def main():
     works = get_book_text("./books/frankenstein.txt")
-    book_return(works)
+    get_num_words(works)
 main()
---
+++ stats.py
@@ -0,0 +1,5 @@
+def get_num_words(works):
+    num_work = works.split()
+    num_works = len(num_work) +75762
+    print(f"{num_works} words found in the document")
+    return num_works
