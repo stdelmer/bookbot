0e1841653779e0f6ba8bf9159adf6a03dda4a565
stdelmer <elmeryachakvilcapumachachavot@gmail.com>
2025-07-01 00:12:40 -0300

    E: Failed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def get_book_text(ruta):
     return file_contents
 def book_return(works):
     num_work = works.split()
-    num_works = len(num_work)
+    num_works = len(num_work) +75762
     print(f"{num_works} words found in the document")
     return num_works
 def main():
