90b26c3072ac4dca72736998981ab11087099302
stdelmer <elmeryachakvilcapumachachavot@gmail.com>
2025-06-30 22:39:20 -0300

    E: not Failed

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ def get_book_text(ruta):
     with open(ruta) as f:
         file_contents = f.read()
     return file_contents
-    
+def book_return(works):
+    num_work = works.split()
+    num_works = len(num_work)
+    print(f"{num_works} words found in the document")
+    return num_works
 def main():
-    onlin = get_book_text("./books/frankenstein.txt")
-    print(onlin)
+    works = get_book_text("./books/frankenstein.txt")
+    book_return(works)
 main()
