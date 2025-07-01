3bdfb62c6c87ce15b21e13e2bcb3909d194465e1
stdelmer <elmeryachakvilcapumachachavot@gmail.com>
2025-06-30 21:46:08 -0300

    E: bien

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(ruta):
+    with open(ruta) as f:
+        file_contents = f.read()
+    return file_contents
+    
+def main():
+    onlin = get_book_text("./books/frankenstein.txt")
+    print(onlin)
+main()
